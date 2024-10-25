ccff244e2577bf5e2fb1b41f17b918bd6812d4b6
Devy <izabela.marcinkowska@outlook.com>
2024-10-25 13:02:51 +0200

    first commit

---
+++ HogwartsArchives/HogwartsArchivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HogwartsArchivesApp.swift
+//  HogwartsArchives
+//
+//  Created by Izabela Marcinkowska on 2024-10-25.
+//
+
+import SwiftUI
+
+@main
+struct HogwartsArchivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
